3bc67f1cf78b6f1b8582776302e6f305aedf2443
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-03-03 23:29:51 -0500

    trying to fix my build system

--- utils/build/build.js
+++ utils/build/build.js
@@ -2,7 +2,6 @@ var fs = require("fs");
 var path = require("path");
 var argparse =  require( "argparse" );
 var uglify = require("uglify-js");
-var spawn = require('child_process').spawn;
 
 function main() {
 
@@ -14,14 +13,14 @@ function main() {
 	parser.addArgument( ['--amd'], { action: 'storeTrue', defaultValue: false } );
 	parser.addArgument( ['--minify'], { action: 'storeTrue', defaultValue: false } );
 	parser.addArgument( ['--output'], { defaultValue: '../../build/three.js' } );
-	parser.addArgument( ['--sourcemaps'], { action: 'storeTrue', defaultValue: true } );
+	parser.addArgument( ['--sourcemaps'], { action: 'storeTrue', defaultValue: false } );
 
-	
+
 	var args = parser.parseArgs();
-	
+
 	var output = args.output;
 	console.log(' * Building ' + output);
-	
+
 	var sourcemap = '';
 	var sourcemapping = '';
 
@@ -38,16 +37,16 @@ function main() {
 	if ( args.amd ){
 		buffer.push('function ( root, factory ) {\n\n\tif ( typeof define === \'function\' && define.amd ) {\n\n\t\tdefine( [ \'exports\' ], factory );\n\n\t} else if ( typeof exports === \'object\' ) {\n\n\t\tfactory( exports );\n\n\t} else {\n\n\t\tfactory( root );\n\n\t}\n\n}( this, function ( exports ) {\n\n');
 	};
-	
+
 	for ( var i = 0; i < args.include.length; i ++ ){
-		
+
 		var contents = fs.readFileSync( './includes/' + args.include[i] + '.json', 'utf8' );
 		var files = JSON.parse( contents );
 
 		for ( var j = 0; j < files.length; j ++ ){
 
 			var file = '../../' + files[ j ];
-			
+
 			buffer.push('// File:' + files[ j ]);
 			buffer.push('\n\n');
 
@@ -67,13 +66,13 @@ function main() {
 		}
 
 	}
-	
+
 	if ( args.amd ){
 		buffer.push('exports.THREE = THREE;\n\n} ) );');
 	};
-	
+
 	var temp = buffer.join( '' );
-	
+
 	if ( !args.minify ){
 
 		fs.writeFileSync( output, temp, 'utf8' );
